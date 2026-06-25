03fc54cd45e0c6fdee0e6dc6687af4c4b7a09d63
dpioli <diegopioli02@gmail.com>
2026-06-25 16:41:20 +0200

    Fix errori cambio password

--- libs/server/auth/src/lib/dto/change-password.dto.ts
+++ libs/server/auth/src/lib/dto/change-password.dto.ts
@@ -2,8 +2,8 @@ import { IsString, MinLength, Matches } from 'class-validator';
 
 export class ChangePasswordDto {
     @IsString()
-    @MinLength(8)
-    @Matches(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
-    @Matches(/[?^!#@]/, { message: 'Password must contain at least one symbol among ? ^ ! # @' })
+    @MinLength(8, { message: 'La password deve contenere almeno 8 caratteri' })
+    @Matches(/[A-Z]/, { message: 'La password deve contenere almeno una lettera maiuscola' })
+    @Matches(/[?^!#@]/, { message: 'La password deve contenere almeno un simbolo tra ? ^ ! # @' })
     newPassword: string;
 }
